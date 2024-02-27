7251c8d003792df5a12e53a89177063ccb8bfe34
artyom74 <artemterentev66@gmail.com>
2024-02-27 23:06:04 +0300

    27.02.2024_Артём Терентьев П311_написать функцую, возращающую периметр   прямоугольника.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// 1. Написать функцию, которая получает на вход стороны прямоугольника, а возвращает площадь. Вызвать функцию в main, результат вывести тоже в main
+// 2. Написать функцию, которая получает на вход стороны прямоугольника, а возвращает периметр.Вызвать функцию в main, результат вывести тоже в main
 
 #include <iostream>
 #include <string>
@@ -6,7 +6,7 @@ using namespace std;
 
 int area(int side1, int side2) {
 
-	int result = (side1 * side2);
+	int result = (side1 + side1) + (side2 + side2);
 	return result;
 }
 
@@ -15,7 +15,7 @@ int main() {
 	setlocale(LC_ALL, "ru");
 
 	int length;
-	cout << "Программа 'подсчёт площади'" << endl << "Введите длину первой стороны: ";
+	cout << "Программа 'подсчёт периметра'" << endl << "Введите длину первой стороны: ";
 	cin >> length;
 
 	int weight;
@@ -23,6 +23,6 @@ int main() {
 	cin >> weight;
 
 	
-	cout << "Площадь вашего прямоугольника равна: " << area(length, weight);
+	cout << "Периметр вашего прямоугольника: " << area(length, weight);
 	return 0;
 } 
